6e18a46d636d7ec675dcbc52a9118a65658d2fde
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2025-06-23 18:24:27 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('React Agent Executor ')
